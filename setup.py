86daefd0b871372253c7a01a3c703169557b8dcf
javierjosev <javierjosev@gmail.com>
2024-03-28 19:26:38 -0300

    Project structure.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Classify Real vs Fake Faces.',
+    author='Bert & Ernie',
+    license='MIT',
+)
